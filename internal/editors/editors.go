9c80eab3992faa617960cbd7f21c534312984266
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-11-21 20:16:26 -0600

    Setup opening gui files

--- internal/editors/editors.go
+++ internal/editors/editors.go
@@ -1,6 +1,7 @@
 package editors
 
 import (
+	"errors"
 	"io"
 	"strings"
 
@@ -10,52 +11,55 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-var extentions = map[string]func(fyne.URI) fyne.CanvasObject{
-	".go":  makeGo,
-	".png": makeImg,
-	".txt": makeTxt,
-	".md":  makeTxt,
+var extentions = map[string]func(fyne.URI) (fyne.CanvasObject, error){
+	".go":   makeGo,
+	".json": makeGUI,
+	".png":  makeImg,
+	".txt":  makeTxt,
+	".md":   makeTxt,
 }
 
-var mimes = map[string]func(fyne.URI) fyne.CanvasObject{
+var mimes = map[string]func(fyne.URI) (fyne.CanvasObject, error){
 	"text/plain": makeTxt,
 }
 
-func ForURI(u fyne.URI) fyne.CanvasObject {
+func ForURI(u fyne.URI) (fyne.CanvasObject, error) {
 	ext := strings.ToLower(u.Extension())
 	edit, ok := extentions[ext]
 	if !ok {
 		edit, ok = mimes[u.MimeType()]
 		if !ok {
-			return widget.NewLabel("Unable to find editor for file " + u.Name() + ", mime " + u.MimeType())
+			return nil, errors.New("Unable to find editor for file " + u.Name() + ", mime " + u.MimeType())
 		}
 	}
 
 	return edit(u)
 }
 
-func makeGo(u fyne.URI) fyne.CanvasObject {
+func makeGo(u fyne.URI) (fyne.CanvasObject, error) {
 	// TODO code editor
-	code := makeTxt(u)
+	code, err := makeTxt(u)
+	if err != nil {
+		return nil, err
+	}
 	code.(*widget.Entry).TextStyle = fyne.TextStyle{Monospace: true}
 
-	return code
+	return code, nil
 }
 
-func makeImg(u fyne.URI) fyne.CanvasObject {
+func makeImg(u fyne.URI) (fyne.CanvasObject, error) {
 	img := canvas.NewImageFromURI(u)
 	img.FillMode = canvas.ImageFillContain
 
-	return img
+	return img, nil
 }
 
-func makeTxt(u fyne.URI) fyne.CanvasObject {
+func makeTxt(u fyne.URI) (fyne.CanvasObject, error) {
 	code := widget.NewEntry()
 
 	r, err := storage.Reader(u)
 	if err != nil {
-		code.SetText("Unable to read " + u.Name())
-		return code
+		return nil, err
 	}
 
 	defer r.Close()
@@ -63,5 +67,5 @@ func makeTxt(u fyne.URI) fyne.CanvasObject {
 	data, _ := io.ReadAll(r)
 	code.SetText(string(data))
 
-	return code
+	return code, nil
 }
